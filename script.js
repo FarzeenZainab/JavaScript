aa91ebfde31217fccb1c487bc9e9ea7435967cf7
FarzeenZainab <farzeenzaidi51@gmail.com>
2024-07-08 09:45:12 +0500

    working with named and default exports and imports

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ console.log('importing Module');
 
 // all importing statements are hoisted to the top
 import './shoppingCart.js';
+import {
+  addToCart,
+  totalPrice,
+  totalQuantity as quantity,
+} from './shoppingCart.js';
+
+// importing everything from inside a module at once
+import * as ShoppingCart from './shoppingCart.js';
+
+addToCart('Apples', 2);
+console.log(totalPrice, quantity);
+console.log(ShoppingCart.totalPrice, ShoppingCart.totalQuantity);
+
+// default import
+import add from './shoppingCart.js';
+add('bread', 5);
+
+// importing named and default exports at once
+import addItem, {
+  addToCart as addNewItem,
+  totalPrice as price,
+} from './shoppingCart.js';
+addItem('oranges', 5);
+addNewItem('banana', 5);
+console.log(price);
--- shoppingCart.js
+++ shoppingCart.js
@@ -4,3 +4,27 @@ console.log('exporting Module');
 
 const shippingCost = 10;
 const cart = [];
+
+// There are two types of exports in ES6 modules
+// Named exports and default exports
+
+// This variable / function is private to this module
+// We will use export so we can use in other modules
+// Named export
+export const addToCart = (product, quantity) => {
+  cart.push(product);
+  console.log(`${quantity} ${product} added to the cart`);
+};
+
+// Exporting multiple variables at the same time
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity };
+
+// We use default exports when we want to export 1 thing per module
+export default (product, quantity) => {
+  cart.push(product);
+  console.log(`${quantity} ${product} added to the cart`);
+};
